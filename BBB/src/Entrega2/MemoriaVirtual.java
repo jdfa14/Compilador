8210f80ebdc675f7927aa5d622dc12fa00583826
Jesus David De La Fuente Amaya <jdfa14@gmail.com>
2015-11-03 22:14:31 -0600

    MemoriaVirtual 1.1
    Agregada la seccion de Constantes y getVar cambiado por tipo de dato

--- BBB/src/Entrega2/MemoriaVirtual.java
+++ BBB/src/Entrega2/MemoriaVirtual.java
@@ -1,14 +1,16 @@
 package Entrega2;
 
-public class MemoriaVirual {
+public class MemoriaVirtual {
     Memoria memG;   // Memoria Global
     Memoria memT;   // Memoria Temporal
     Memoria memL;   // Memoria Local
+    Memoria memC;   // Memoria Constante
     
-    public MemoriaVirual(){
+    public MemoriaVirtual(){
         memG = new Memoria();
         memT = new Memoria();
         memL = new Memoria();
+        memC = new Memoria();
     }
     
     public void wipeLocal(){
@@ -55,38 +57,77 @@ public class MemoriaVirual {
             case TEMPORAL_BOOLEAN:
                 memT.saveBoolean(dir, (boolean) value);
                 break;
+            case CONSTANT_INT:
+                memC.saveInt(dir, (int)value);
+                break;
+            case CONSTANT_FLOAT:
+                memC.saveFloat(dir, (double) value);
+                break;
+            case CONSTANT_STRING: 
+                memC.saveString(dir, (String) value);
+                break;
+            case CONSTANT_BOOLEAN:
+                memC.saveBoolean(dir, (boolean) value);
+                break;
         }
     }
-    
-    public Object getVar(int dir){  // Obtener el valor de una direccion
+    public String getString(int dir){
         Memoria.TYPE type = Memoria.getType(dir);
-        
         switch(type){
-            case GLOBAL_INT:
-                return memG.getInt(dir);
-            case GLOBAL_FLOAT:
-                return memG.getFloat(dir);
             case GLOBAL_STRING: 
                 return memG.getString(dir);
-            case GLOBAL_BOOLEAN:
-                return memG.getBoolean(dir);
-            case LOCAL_INT:
-                return memL.getInt(dir);
-            case LOCAL_FLOAT:
-                return memL.getFloat(dir);
             case LOCAL_STRING: 
                 return memL.getString(dir);
-            case LOCAL_BOOLEAN:
-                return memL.getBoolean(dir);
-            case TEMPORAL_INT:
-                return memT.getInt(dir);
-            case TEMPORAL_FLOAT:
-                return memT.getFloat(dir);
             case TEMPORAL_STRING: 
                 return memT.getString(dir);
-            case TEMPORAL_BOOLEAN:
-                return memT.getBoolean(dir);
+            case CONSTANT_STRING:
+                return memC.getString(dir);
         }
-        return null;
+        return null; // ERROR
+    }
+    
+    public int getInt(int dir){
+        Memoria.TYPE type = Memoria.getType(dir);
+        switch(type){
+            case GLOBAL_INT: 
+                return memG.getInt(dir);
+            case LOCAL_INT: 
+                return memL.getInt(dir);
+            case TEMPORAL_INT: 
+                return memT.getInt(dir);
+            case CONSTANT_INT:
+                return memC.getInt(dir);
+        }
+        return -1; // MOSTRAR ERROR
+    }
+    
+    public double getFloat(int dir){
+        Memoria.TYPE type = Memoria.getType(dir);
+        switch(type){
+            case GLOBAL_FLOAT: 
+                return memG.getFloat(dir);
+            case LOCAL_FLOAT: 
+                return memL.getFloat(dir);
+            case TEMPORAL_FLOAT: 
+                return memT.getFloat(dir);
+            case CONSTANT_FLOAT:
+                return memC.getFloat(dir);
+        }
+        return -1; // MOSTRAR ERROR
+    }
+    
+    public boolean getBoolean(int dir){
+        Memoria.TYPE type = Memoria.getType(dir);
+        switch(type){
+            case GLOBAL_BOOLEAN: 
+                return memG.getBoolean(dir);
+            case LOCAL_BOOLEAN: 
+                return memL.getBoolean(dir);
+            case TEMPORAL_BOOLEAN: 
+                return memT.getBoolean(dir);
+            case CONSTANT_BOOLEAN:
+                return memC.getBoolean(dir);
+        }
+        return false; // MOSTRAR ERROR
     }
 }
